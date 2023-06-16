28b73c5514e62188d610c9a69c502219abab5dc5
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-06-16 23:06:50 +0300

    Stack implementation

--- Linear-Data-Structures/src/main/java/Main.java
+++ Linear-Data-Structures/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import implementations.Stack;
+
 public class Main {
 	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
 
+		stack.push(5);
+		stack.push(6);
+		stack.push(7);
+
+		System.out.println(stack.peek());
+
+		System.out.println(stack.pop());
+
+		System.out.println(stack.peek());
 	}
 }
--- Linear-Data-Structures/src/main/java/implementations/Stack.java
+++ Linear-Data-Structures/src/main/java/implementations/Stack.java
@@ -1,38 +1,95 @@
 package implementations;
 
-import interfaces.AbstractStack;
-
 import java.util.Iterator;
 
+import interfaces.AbstractStack;
+
 public class Stack<E> implements AbstractStack<E> {
+	private Node topElement;
+	private int size;
 
-    @Override
-    public void push(E element) {
+	public Stack() {
+		this.size = 0;
+	}
 
-    }
+	private final class Node {
+		private E value;
+		private Node prev;
 
-    @Override
-    public E pop() {
-        return null;
-    }
+		private Node(E value) {
+			this.value = value;
+			this.prev = null;
+		}
+	}
 
-    @Override
-    public E peek() {
-        return null;
-    }
+	@Override
+	public void push(E element) {
+		Node node = new Node(element);
+		
+		if (size == 0) {
+			this.topElement = node;
 
-    @Override
-    public int size() {
-        return 0;
-    }
+		} else {
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
+			node.prev = this.topElement;
+			this.topElement = node;
 
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
+		}
+		size++;
+	}
+
+	@Override
+	public E pop() {
+		if (this.isEmpty()) {
+			throw new IllegalStateException("You can't remove elements from empty stack!");
+		}
+
+		E removedElement = topElement.value;
+		this.topElement = this.topElement.prev;
+		size--;
+
+		return removedElement;
+	}
+
+	@Override
+	public E peek() {
+		if (isEmpty()) {
+			throw new IllegalStateException("You can't peek empty stack!");
+		}
+
+		return this.topElement.value;
+	}
+
+	@Override
+	public int size() {
+		return this.size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return new StackIterator();
+	}
+
+	private final class StackIterator implements Iterator<E> {
+		Node top = topElement;
+
+		@Override
+		public boolean hasNext() {
+			return top.prev != null;
+		}
+
+		@Override
+		public E next() {
+			E val = top.value;
+			top = top.prev;
+			
+			return val;
+		}
+
+	}
 }
