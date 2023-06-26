e1b138991dabf3cd4baf3fe31c752908a75a2233
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-06-26 12:54:59 +0300

    Priority queue implementation

--- Binaty-Trees-And-Heaps/src/main/java/implementations/PriorityQueue.java
+++ Binaty-Trees-And-Heaps/src/main/java/implementations/PriorityQueue.java
@@ -1,30 +1,79 @@
 package implementations;
 
-import interfaces.AbstractQueue;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import interfaces.AbstractQueue;
+
 public class PriorityQueue<E extends Comparable<E>> implements AbstractQueue<E> {
+	private List<E> elements;
 
-    @Override
-    public int size() {
-        return 0;
-    }
+	public PriorityQueue() {
+		this.elements = new ArrayList<>();
+	}
 
-    @Override
-    public void add(E element) {
+	@Override
+	public int size() {
+		return this.elements.size();
+	}
 
-    }
+	@Override
+	public void add(E element) {
+		this.elements.add(0, element);
 
-    @Override
-    public E peek() {
-        return null;
-    }
+		heapifyDown(0);
 
-    @Override
-    public E poll() {
-        return null;
-    }
+	}
+
+	@Override
+	public E peek() {
+		ensureNonEmpty();
+
+		return this.elements.get(0);
+	}
+
+	@Override
+	public E poll() {
+		ensureNonEmpty();
+
+		E element = this.elements.get(0);
+		Collections.swap(this.elements, 0, this.size() - 1);
+		this.elements.remove(this.size() - 1);
+		this.heapifyDown(0);
+
+		return element;
+	}
+
+	private void heapifyDown(int index) {
+		while (getLeftChildIndex(index) < this.size() && isLess(index, getLeftChildIndex(index))) {
+			int childIndex = getLeftChildIndex(index);
+			int rightChildIndex = getRightChildIndex(index);
+
+			if (rightChildIndex < this.size() && isLess(childIndex, rightChildIndex)) {
+				childIndex = rightChildIndex;
+			}
+
+			Collections.swap(this.elements, childIndex, index);
+			index = childIndex;
+		}
+	}
+
+	private boolean isLess(int first, int second) {
+		return this.elements.get(first).compareTo(this.elements.get(second)) < 0;
+	}
+
+	private void ensureNonEmpty() {
+		if (this.size() == 0) {
+			throw new IllegalStateException("Can't peek or poll from empty heap!");
+		}
+	}
+
+	private int getLeftChildIndex(int index) {
+		return 2 * index + 1;
+	}
+
+	private int getRightChildIndex(int index) {
+		return 2 * index + 2;
+	}
 }
--- Binaty-Trees-And-Heaps/src/main/java/interfaces/AbstractBinarySearchTree.java
+++ Binaty-Trees-And-Heaps/src/main/java/interfaces/AbstractBinarySearchTree.java
@@ -1,29 +1,33 @@
 package interfaces;
 
-import implementations.BinarySearchTree;
-
 public interface AbstractBinarySearchTree<E extends Comparable<E>> {
-    public static class Node<E> {
-        public E value;
-        public Node<E> leftChild;
-        public Node<E> rightChild;
+	public static class Node<E> {
+		public E value;
+		public Node<E> leftChild;
+		public Node<E> rightChild;
 
-        public Node(E value) {
-            this.value = value;
-        }
+		public Node(E value) {
+			this.value = value;
+		}
 
-        public Node(E value, Node<E> leftChild, Node<E> rightChild) {
-            this.value = value;
-            this.leftChild = leftChild;
-            this.rightChild = rightChild;
-        }
-    }
+		public Node(E value, Node<E> leftChild, Node<E> rightChild) {
+			this.value = value;
+			this.leftChild = leftChild;
+			this.rightChild = rightChild;
+		}
+	}
 
-    void insert(E element);
-    boolean contains(E element);
-    AbstractBinarySearchTree<E> search(E element);
-    Node<E> getRoot();
-    Node<E> getLeft();
-    Node<E> getRight();
-    E getValue();
+	void insert(E element);
+
+	boolean contains(E element);
+
+	AbstractBinarySearchTree<E> search(E element);
+
+	Node<E> getRoot();
+
+	Node<E> getLeft();
+
+	Node<E> getRight();
+
+	E getValue();
 }
