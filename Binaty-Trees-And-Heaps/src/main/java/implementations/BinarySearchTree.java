7bf5b563f128b8e1f8b9a23f171b3a0c4859ab92
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-06-26 18:18:36 +0300

    Binary Search Tree - implementation

--- Binaty-Trees-And-Heaps/src/main/java/implementations/BinarySearchTree.java
+++ Binaty-Trees-And-Heaps/src/main/java/implementations/BinarySearchTree.java
@@ -3,39 +3,118 @@ package implementations;
 import interfaces.AbstractBinarySearchTree;
 
 public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinarySearchTree<E> {
+	private Node<E> root;
 
-    @Override
-    public void insert(E element) {
+	public BinarySearchTree() {
 
-    }
+	}
 
-    @Override
-    public boolean contains(E element) {
-        return false;
-    }
+	public BinarySearchTree(Node<E> root) {
+		this.copy(root);
+	}
 
-    @Override
-    public AbstractBinarySearchTree<E> search(E element) {
-        return null;
-    }
+	private void copy(Node<E> node) {
+		if (node != null) {
+			this.insert(node.value);
+			this.copy(node.leftChild);
+			this.copy(node.rightChild);
+		}
+	}
 
-    @Override
-    public Node<E> getRoot() {
-        return null;
-    }
+	@Override
+	public void insert(E element) {
+		Node<E> newNode = new Node<>(element);
 
-    @Override
-    public Node<E> getLeft() {
-        return null;
-    }
+		if (this.getRoot() == null) {
+			this.root = newNode;
 
-    @Override
-    public Node<E> getRight() {
-        return null;
-    }
+		} else {
+			Node<E> current = this.root;
+			Node<E> prev = null;
 
-    @Override
-    public E getValue() {
-        return null;
-    }
+			while (current != null) {
+				prev = current;
+
+				if (isLess(element, current.value)) {
+					current = current.leftChild;
+
+				} else if (isGreated(element, current.value)) {
+					current = current.rightChild;
+				}
+			}
+			current = newNode;
+
+			if (isLess(element, prev.value)) {
+				prev.leftChild = current;
+
+			} else if (isGreated(element, prev.value)) {
+				prev.rightChild = current;
+			}
+		}
+
+	}
+
+	private boolean isLess(E first, E second) {
+		return first.compareTo(second) < 0;
+	}
+
+	private boolean isGreated(E first, E second) {
+		return first.compareTo(second) > 0;
+	}
+
+	@Override
+	public boolean contains(E element) {
+		Node<E> current = this.getRoot();
+
+		while (current != null) {
+			if (isLess(element, current.value)) {
+				current = current.leftChild;
+
+			} else if (isGreated(element, current.value)) {
+				current = current.rightChild;
+
+			} else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public AbstractBinarySearchTree<E> search(E element) {
+		Node<E> current = this.getRoot();
+
+		while (current != null) {
+			if (isLess(element, current.value)) {
+				current = current.leftChild;
+
+			} else if (isGreated(element, current.value)) {
+				current = current.rightChild;
+
+			} else {
+				return new BinarySearchTree<>(current);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Node<E> getRoot() {
+		return this.root;
+	}
+
+	@Override
+	public Node<E> getLeft() {
+		return this.root.leftChild;
+	}
+
+	@Override
+	public Node<E> getRight() {
+		return this.root.rightChild;
+	}
+
+	@Override
+	public E getValue() {
+		return this.root.value;
+	}
 }
