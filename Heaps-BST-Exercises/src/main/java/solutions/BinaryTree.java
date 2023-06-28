7be53d6429f23d352cb5674036440c195d7f2766
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-06-28 21:58:24 +0300

    Lowest Common Ancestor - soution

--- Heaps-BST-Exercises/src/main/java/solutions/BinaryTree.java
+++ Heaps-BST-Exercises/src/main/java/solutions/BinaryTree.java
@@ -1,18 +1,60 @@
 package solutions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTree {
+	private int key;
+	private BinaryTree left;
+	private BinaryTree right;
 
-    public BinaryTree(int key, BinaryTree first, BinaryTree second) {
+	public BinaryTree(int key, BinaryTree first, BinaryTree second) {
+		this.key = key;
+		this.left = first;
+		this.right = second;
+	}
 
-    }
+	public Integer findLowestCommonAncestor(int first, int second) {
+		List<Integer> firstPath = findPath(first);
+		List<Integer> secondPath = findPath(second);
 
-    public Integer findLowestCommonAncestor(int first, int second) {
-        return null;
-    }
+		int smallerSize = Math.min(firstPath.size(), secondPath.size());
 
-    public List<Integer> topView() {
-        return null;
-    }
+		int i = 0;
+		for (; i < smallerSize; i++) {
+			if (!firstPath.get(i).equals(secondPath.get(i))) {
+				break;
+			}
+		}
+
+		return firstPath.get(i - 1);
+	}
+
+	private List<Integer> findPath(int element) {
+		List<Integer> result = new ArrayList<>();
+
+		findNodePath(this, element, result);
+
+		return result;
+	}
+
+	private void findNodePath(BinaryTree binaryTree, int element, List<Integer> result) {
+		if(binaryTree == null) {
+			return;
+		}
+		
+		if(binaryTree.key == element) {
+			return;
+		}
+		
+		result.add(binaryTree.key);
+		
+		findNodePath(binaryTree.left, element, result);
+		findNodePath(binaryTree.right, element, result);
+		
+	}
+
+	public List<Integer> topView() {
+		return null;
+	}
 }
